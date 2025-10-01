97b8eae727ba276352279dab5f84373a08497980
adsuel <adamwlodyka07@gmail.com>
2025-10-01 12:53:30 +0000

    nsghs

--- main.tf
+++ main.tf
@@ -34,4 +34,17 @@ resource "azurerm_linux_web_app" "example" {
   resource_group_name = "rg-06"
   service_plan_id     = azurerm_service_plan.example.id
   site_config {}
+  provisioner "local-exec" {
+    command = <<EOT
+      az webapp deployment source config-zip \
+        --resource-group ${self.resource_group_name} \
+        --name ${self.name} \
+        --src pong.zip
+    EOT
+  }
+}
+resource "null_resource" "zip_pong" {
+  provisioner "local-exec" {
+    command = "zip -j pong.zip pong.html"
+  }
 }
